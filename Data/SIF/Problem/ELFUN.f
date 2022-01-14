b23137303e02b316fde6a72904ff5727ad949f86
Jonas <jonasbhjulstad@gmail.com>
2022-01-14 04:17:53 +0100

    Cleanup

--- Data/SIF/Problem/ELFUN.f
+++ Data/SIF/Problem/ELFUN.f
@@ -11,23 +11,38 @@
       INTEGER ICALCF(LCALCF)
       DOUBLE PRECISION FUVALS(LFVALU), XVALUE(LXVALU), EPVALU(LEPVLU)
 C
-C  Problem name : HS35      
+C  Problem name : HS13      
 C
 C  -- produced by SIFdecode 1.0
 C
       INTEGER IELEMN, IELTYP, IHSTRT, ILSTRT, IGSTRT, IPSTRT
       INTEGER JCALCF
-      DOUBLE PRECISION V1    , V2    
+      DOUBLE PRECISION V1    , WMV1  , V1M2  
       IFSTAT = 0
-      DO     3 JCALCF = 1, NCALCF
+      DO     4 JCALCF = 1, NCALCF
        IELEMN = ICALCF(JCALCF) 
        ILSTRT = ISTAEV(IELEMN) - 1
        IGSTRT = INTVAR(IELEMN) - 1
        IPSTRT = ISTEPA(IELEMN) - 1
        IF ( IFFLAG == 3 ) IHSTRT = ISTADH(IELEMN) - 1
        IELTYP = ITYPEE(IELEMN)
-       GO TO (    1,    2
+       GO TO (    1,    2,    3
      *                                                        ), IELTYP
+C
+C  Element type : SQ-2      
+C
+    2  CONTINUE
+       V1     = XVALUE(IELVAR(ILSTRT+     1))
+       V1M2   = V1 - 2.0                                 
+       IF ( IFFLAG == 1 ) THEN
+        FUVALS(IELEMN)= (V1M2)**2                                
+       ELSE
+        FUVALS(IGSTRT+     1)= 2.0*(V1M2)                               
+        IF ( IFFLAG == 3 ) THEN
+         FUVALS(IHSTRT+     1)=2.0                                      
+        END IF
+       END IF
+       GO TO     4
 C
 C  Element type : SQ        
 C
@@ -41,24 +56,21 @@ C
          FUVALS(IHSTRT+     1)=2.0                                      
         END IF
        END IF
-       GO TO     3
+       GO TO     4
 C
-C  Element type : 2PR       
+C  Element type : CUBE      
 C
-    2  CONTINUE
+    3  CONTINUE
        V1     = XVALUE(IELVAR(ILSTRT+     1))
-       V2     = XVALUE(IELVAR(ILSTRT+     2))
+       WMV1   = 1.0 - V1                                 
        IF ( IFFLAG == 1 ) THEN
-        FUVALS(IELEMN)= V1 * V2                                  
+        FUVALS(IELEMN)= WMV1**3                                  
        ELSE
-        FUVALS(IGSTRT+     1)= V2                                       
-        FUVALS(IGSTRT+     2)= V1                                       
+        FUVALS(IGSTRT+     1)= -3.0 * WMV1 * WMV1                       
         IF ( IFFLAG == 3 ) THEN
-         FUVALS(IHSTRT+     2)=1.0                                      
-         FUVALS(IHSTRT+     1)=0.0D+0
-         FUVALS(IHSTRT+     3)=0.0D+0
+         FUVALS(IHSTRT+     1)=6.0 * WMV1                               
         END IF
        END IF
-    3 CONTINUE
+    4 CONTINUE
       RETURN
       END
